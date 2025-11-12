89b6e11e7407767a809f6ab684d0c79b75d16f80
diegom1610 <diegom1610@gmail.com>
2025-11-11 20:25:00 -0500

    removed create-user.html from repository and  added vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  publicDir: 'public', // This is the default, but let's be explicit
 });
